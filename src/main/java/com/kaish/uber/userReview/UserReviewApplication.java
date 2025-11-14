d14ea1db48a5be2ee7605f57f8d1f31f09135072
Kaish Reza <kaishreza786@gmail.com>
2025-11-15 00:10:12 +0530

    created review microservice

---
+++ src/main/java/com/kaish/uber/userReview/UserReviewApplication.java
@@ -0,0 +1,13 @@
+package com.kaish.uber.userReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserReviewApplication.class, args);
+	}
+
+}
